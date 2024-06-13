d2ed337f9eeed272fa5ef2aa6b62568f973ca05d
FalsoMoralista <lucianoadfilho@gmail.com>
2024-06-13 16:48:22 -0300

    assignment

--- tests/test_K_means_module.py
+++ tests/test_K_means_module.py
@@ -18,12 +18,12 @@ def main():
 
     x = torch.randn(batch_size, input_dim, dtype=torch.float32)  # Batch of 64 samples, ensure faiss float32    
     y = torch.randint(low=0, high=(input_dim-1), size=[batch_size])    
+    print(x[0].size())
+    print(y.size())
 
     D,I = kmeans.assign(x,y)
-    print(D)
-    print(I)
-
-
-
+    print(D.size())
+    print(I.size())
+    
 main()
 
